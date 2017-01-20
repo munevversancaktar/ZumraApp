81bf16fa67ca69d0d6ba198f47aa406802828f53
MUNEVVER SANCAKTAR <munevversancaktar@gmail.com>
2017-01-20 17:50:42 +0300

    commit third

--- ZumraApp.Data.Model/Models/Images/Image.cs
+++ ZumraApp.Data.Model/Models/Images/Image.cs
@@ -8,6 +8,24 @@ namespace ZumraApp.Data.Model.Models.Images
 {
     public class Image
     {
+        public int ImageID { get; set; }
 
+        public string Name { get; set; }
+
+        public string LittleImageWay { get; set; }
+
+        public string MiddleImageWay { get; set; }
+
+        public string BigImageWay { get; set; }
+
+        public string VideoWay { get; set; }
+
+        public Guid AddingID { get; set; }
+
+        public DateTime AddingDate { get; set; }
+
+        public int Seen { get; set; }
+
+        public int Like { get; set; }
     }
 }
